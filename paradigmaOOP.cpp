043ad2566c3a95a3fdba1397d550c44435840af2
pamiii12 <fahmiadityaa03@gmail.com>
2023-05-24 15:13:20 +0700

    class orang

--- paradigmaOOP.cpp
+++ paradigmaOOP.cpp
@@ -2,4 +2,19 @@
 #include <string>
 using namespace std;
 
-class orang
+class orang {
+public:
+	string nama;
+
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "orang dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "orang dihapus\n" << endl;
+	}
+
+	int jumlah(int a, int b) {
+		return a + b;
+	}
+};
