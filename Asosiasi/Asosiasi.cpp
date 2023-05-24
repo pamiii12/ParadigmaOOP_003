b0611fe6a6151da1dc2a2ed664873b4a0744374c
pamiii12 <fahmiadityaa03@gmail.com>
2023-05-24 15:48:58 +0700

    13

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -24,6 +24,16 @@ public:
 	string nama;
 	vector<pasien*> daftar_pasien;
 
-
+	dokter(string pNama) :nama(pNama) {
+		cout << "Dokter \"" << nama << "\" ada\n";
+	}
+	~dokter() {
+		cout << "Dokter \"" << nama << "\" tidak ada\n";
+	}
+	~dokter() {
+		cout << "Dokter \"" << nama << "\" tidak ada\n";
+	}
+	void tambahPasien(pasien*);
+	void cetakPasien();
 };
 
